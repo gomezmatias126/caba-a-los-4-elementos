b7127bd3783a66ca72203590e6cf21a9fc03001c
Matias Gomez <gomezmatias126@gmail.com>
2026-02-13 12:33:15 -0300

    agregado boton whatsapp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,13 @@
 <script>
 	import Hero from '$lib/components/Hero.svelte';
-    import FiltroEtico from '$lib/components/FiltroEtico.svelte';
-    import Unidades from '$lib/components/Unidades.svelte';
-    import GuiaExplorador from '$lib/components/GuiaExplorador.svelte';
-    import PoliticasEstancia from '$lib/components/PoliticasEstancia.svelte';
-    import Testimonios from '$lib/components/Testimonios.svelte';
-    import Formulario from '$lib/components/Formulario.svelte';
-    let unidadSeleccionada = $state("");
+	import FiltroEtico from '$lib/components/FiltroEtico.svelte';
+	import Unidades from '$lib/components/Unidades.svelte';
+	import GuiaExplorador from '$lib/components/GuiaExplorador.svelte';
+	import PoliticasEstancia from '$lib/components/PoliticasEstancia.svelte';
+	import Testimonios from '$lib/components/Testimonios.svelte';
+	import Formulario from '$lib/components/Formulario.svelte';
+	import Whatsapp from '$lib/components/Whatsapp.svelte';
+	let unidadSeleccionada = $state('');
 </script>
 
 <svelte:head>
@@ -14,10 +15,11 @@
 </svelte:head>
 
 <!-- <Unidades2 /> -->
+<Whatsapp />
 <Hero />
 <FiltroEtico />
-<Unidades bind:unidadSeleccionada/>
+<Unidades bind:unidadSeleccionada />
 <GuiaExplorador />
 <PoliticasEstancia />
 <Testimonios />
-<Formulario bind:unidadSeleccionada/>
+<Formulario bind:unidadSeleccionada />
